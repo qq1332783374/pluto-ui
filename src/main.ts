40c16290a32663b20fb35d0e1afba3558d69d5cc
qq1332783374 <1332783374@qq.com>
2020-10-22 23:42:50 +0800

    添加组件整体样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import {router} from "./router";
 import App from './App.vue'
 import './index.scss'
+import '../packages/theme/pluto-ui.scss'
 import '//at.alicdn.com/t/font_2142549_dpth86gk84.js'
 
 createApp(App)
