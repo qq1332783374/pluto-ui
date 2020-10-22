c6b75aaef80aae18a546fd4ad7b988474b058f06
qq1332783374 <1332783374@qq.com>
2020-10-22 23:32:24 +0800

    添加button组件基础架子

---
+++ packages/Button/index.ts
@@ -0,0 +1,5 @@
+import PButton from './src/P-Button.vue'
+
+PButton.install = (Vue) => Vue.component(PButton.name, PButton)
+
+export default PButton
---
+++ packages/Button/src/P-Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>按钮</button>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'PButton'
+  })
+</script>
+
+<style lang="scss">
+
+</style>
