7a32c02dd065dd00118788503416a0be57591b75
qq1332783374 <1332783374@qq.com>
2020-10-27 00:38:08 +0800

    添加button组件样式

--- packages/Button/src/P-Button.vue
+++ packages/Button/src/P-Button.vue
@@ -1,12 +1,20 @@
 <template>
-  <button>按钮</button>
+  <button class="pluto-button pluto-button--success">
+    <slot>按钮</slot>
+  </button>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
 
   export default defineComponent({
-    name: 'PButton'
+    name: 'PButton',
+    props: {
+      theme: {
+        type: String,
+        default: 'default'
+      }
+    }
   })
 </script>
 
